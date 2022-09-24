e9d0d1ee0aafb317266ad86de4aab04bb2fb9141
Talk2David1 <107730513+Talk2David1@users.noreply.github.com>
2022-09-24 11:53:38 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function validation()
+{
+    let form = document.getElementById("form");
+    let email = document.getElementById("email").value;
+    let text = document.getElementById("text");
+    let pattern =  /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern))
+    {
+      form.classList.add("valid");
+      form.classList.remove("invalid");
+      text.innerHTML = "Your Email Address is Valid";
+      text.style.color = "#00ff00";
+    }
+    else
+    {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "please provide a valid email address";
+        text.style.color = "#ff0000";
+   
+    }
+
+    if (email == "")
+    {
+      form.classList.remove("valid");
+      form.classList.remove("invalid");
+      text.innerHTML = "please input your email address";
+      text.style.color = "#ff0000";
+    }
+}
